649a39b2f7ad7737db23893c2553418f2b30d483
aryaun xames <ariankhamesi@gmail.com>
2024-01-06 23:10:28 +0330

    Adding props to breakcrumb component and making it dynamic

--- sabzlearn/frontend/src/Components/BreadCrumb/BreadCrumb.jsx
+++ sabzlearn/frontend/src/Components/BreadCrumb/BreadCrumb.jsx
@@ -1,36 +1,29 @@
 import React from 'react'
 
 import './BreadCrumb.css'
+import { Link } from 'react-router-dom'
 
-export default function BreadCrumb() {
-  return (
-    <section class="breadcrumb">
-      <div class="container">
-        <div class="breadcrumb__content">
-          <div class="breadcrumb__home-content-icon">
-            <i class="fas fa-home breadcrumb__home-icon"></i>
-          </div>
-          <ul class="breadcrumb__list">
-            <li class="breadcrumb__item">
-              <a href="#" class="breadcrumb__link">
-                خانه
-                <i class="fas fa-angle-left breadcrumb__icon"></i>
-              </a>
-            </li>
-            <li class="breadcrumb__item">
-              <a href="#" class="breadcrumb__link">
-                آموزش برنامه نویسی فرانت اند
-                <i class="fas fa-angle-left breadcrumb__icon"></i>
-              </a>
-            </li>
-            <li class="breadcrumb__item">
-              <a href="#" class="breadcrumb__link">
-                دوره متخصص جاوا اسکریپت
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-  )
+export default function BreadCrumb({ links }) {
+    return (
+        <section class="breadcrumb">
+            <div class="container">
+                <div class="breadcrumb__content">
+                    <div class="breadcrumb__home-content-icon">
+                        <i class="fas fa-home breadcrumb__home-icon"></i>
+                    </div>
+                    <ul class="breadcrumb__list">
+
+                        {links.map((link)=>(
+                            <li class="breadcrumb__item">
+                            <Link to={link.to} class="breadcrumb__link">
+                                {link.title}
+                                <i class="fas fa-angle-left breadcrumb__icon"></i>
+                            </Link>
+                        </li>
+                       ) )}
+                    </ul>
+                </div>
+            </div>
+        </section>
+    )
 }
--- sabzlearn/frontend/src/Pages/CourseInfo/CourseInfo.jsx
+++ sabzlearn/frontend/src/Pages/CourseInfo/CourseInfo.jsx
@@ -11,7 +11,14 @@ export default function CourseInfo() {
     <>
     <TopBar/>
     <NavBar/>
-    <BreadCrumb/>
+    <BreadCrumb
+    links={
+      [
+        {id:1,title:'خانه',to:'/'},
+        {id:2,title:'آموزش برنامه نویسی فرانت اند',to:'/category-info/frontend'},
+        {id:3,title:'دوره متخصص جاوااسکریپت',to:'/course-info/js-expert'},
+      ]
+    }/>
     
     <Footer/>
     </>
