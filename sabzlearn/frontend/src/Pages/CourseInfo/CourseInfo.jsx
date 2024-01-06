cb5b21b4279beaf643095afa8df5fdf6c8de799f
aryaun xames <ariankhamesi@gmail.com>
2024-01-06 22:26:12 +0330

    Start building the course info page and add the ready top bar and footer to it and build the breadcrumbs component

--- sabzlearn/frontend/src/Pages/CourseInfo/CourseInfo.jsx
+++ sabzlearn/frontend/src/Pages/CourseInfo/CourseInfo.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 import "./CourseInfo.css"
+import TopBar from '../../Components/TopBar/TopBar'
+import NavBar from '../../Components/NavBar/NavBar'
+import Footer from '../../Components/Footer/Footer'
+import BreadCrumb from '../../Components/BreadCrumb/BreadCrumb'
 
 export default function CourseInfo() {
   return (
-    <div>CourseInfo</div>
+    <>
+    <TopBar/>
+    <NavBar/>
+    <BreadCrumb/>
+    
+    <Footer/>
+    </>
   )
 }
