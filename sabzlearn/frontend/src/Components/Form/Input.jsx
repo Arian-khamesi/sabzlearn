1fb37d693927b18003fd6866a895588ddccf2085
aryaun xames <ariankhamesi@gmail.com>
2024-01-12 14:48:33 +0330

    add onChange method to all inputs

--- sabzlearn/frontend/src/Components/Form/Input.jsx
+++ sabzlearn/frontend/src/Components/Form/Input.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 import './Input.css'
 
-export default function Input( props ) {
+export default function Input(props) {
+
+    const inputChangeHandler = (event) => {
+        console.log(event.target.value)
+    }
 
     const children = props.element === 'input' ? (
         <input
             type={props.type}
             placeholder={props.placeholder}
             className={props.className}
+            onChange={inputChangeHandler}
         />
     ) : (<textarea
         placeholder={props.placeholder}
