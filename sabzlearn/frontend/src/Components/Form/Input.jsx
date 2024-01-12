6695ed7de79a6bd451958399f55999775c1fc3f1
aryaun xames <ariankhamesi@gmail.com>
2024-01-12 15:14:23 +0330

    add reducer to manage states in input component

--- sabzlearn/frontend/src/Components/Form/Input.jsx
+++ sabzlearn/frontend/src/Components/Form/Input.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
 import './Input.css'
 
 export default function Input(props) {
 
+    const inputReducer = (state, action) => {
+        switch (action.type) {
+            case 'CHANGE': {
+                return {
+                    ...state,
+                    value: action.value,
+                    isValid: action.isValid
+                }
+            }
+            default: {
+                return state
+            }
+        }
+    }
+
+    const [maininput, dispatch] = useReducer(inputReducer,
+        {
+            value: '',
+            isValid: false
+        })
+
     const inputChangeHandler = (event) => {
-        console.log(event.target.value)
+        dispatch({
+            type: 'CHANGE',
+            value: event.target.value,
+            isValid: true
+        })
     }
 
     const children = props.element === 'input' ? (
         <input
             type={props.type}
             placeholder={props.placeholder}
-            className={props.className}
+            className={`${props.className} ${maininput.isValid ? ('success') : ('error')}`}
+            value={maininput.value}
             onChange={inputChangeHandler}
         />
     ) : (<textarea
         placeholder={props.placeholder}
         className={props.className}
+        value={maininput.value}
+        onChange={inputChangeHandler}
     />)
 
 
