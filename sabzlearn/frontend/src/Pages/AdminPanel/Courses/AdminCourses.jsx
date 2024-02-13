7293aeb9d04a7ca462f358b4b9217e2ea814ea9e
aryaun xames <ariankhamesi@gmail.com>
2024-02-13 19:03:21 +0330

    get category ID to courses admin panel component to add new course

--- sabzlearn/frontend/src/Pages/AdminPanel/Courses/AdminCourses.jsx
+++ sabzlearn/frontend/src/Pages/AdminPanel/Courses/AdminCourses.jsx
@@ -55,8 +55,159 @@ export default function Courses() {
       .then(result => getCourses())
   }
 
+
+  /////////////////////////////get category id//////////////////
+
+  const [allCategory, setAllCategory] = useState([])
+  const [courseCategory, setCourseCategory] = useState("");
+
+  useEffect(() => {
+    getAllCategories()
+  }, [])
+
+
+  const getAllCategories = () => {
+    fetch("http://localhost:5000/v1/category")
+      .then(res => res.json())
+      .then(result => setAllCategory(result))
+  }
+
+  const selectCategory = (event) => {
+    console.log(event.target.value);
+    setCourseCategory(event.target.value);
+  }
+
   return (
     <>
+
+      <div class="container-fluid" id="home-content">
+        <div class="container">
+          <div class="home-title">
+            <span>افزودن محصول جدید</span>
+          </div>
+          <form class="form">
+            <div class="col-6">
+              <div class="name input">
+                <label class="input-title">نام محصول</label>
+                <input
+                  type="text"
+                  isValid="false"
+                  placeholder="لطفا نام محصول را وارد کنید..."
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="price input">
+                <label class="input-title">قیمت محصول</label>
+                <input
+                  type="text"
+                  isValid="false"
+                  placeholder="لطفا قیمت محصول را وارد کنید..."
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="number input">
+                <label class="input-title">تعداد محصول</label>
+                <input
+                  type="text"
+                  isValid="false"
+                  placeholder="لطفا تعداد محصول را وارد کنید..."
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="price input">
+                <label class="input-title">قیمت محصول</label>
+                <input
+                  type="text"
+                  isValid="false"
+                  placeholder="لطفا قیمت محصول را وارد کنید..."
+                />
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="number input">
+                <label className="input-title">دسته‌بندی دوره</label>
+                <select onChange={selectCategory}>
+                  {allCategory.map((category) => (
+                    <option value={category._id}>{category.title}</option>
+                  ))}
+                </select>
+                <span class="error-message text-danger"></span>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="file">
+                <label class="input-title">عکس محصول</label>
+                <input type="file" id="file" />
+              </div>
+            </div>
+            <div class="col-6 midlle">
+              <div class="presell">
+                <label class="input-title">وضعیت دوره</label>
+                <div class="radios">
+                  <div class="presell-true">
+                    <label>
+                      <span>پیش فروش</span>
+                      <input
+                        type="radio"
+                        value="presell"
+                        name="presell"
+                        checked
+                      />
+                    </label>
+                  </div>
+                  <div class="presell-false">
+                    <label>
+                      <span>در حال برگزاری</span>
+                      <input type="radio" value="onperforming" name="presell" />
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6 midlle">
+              <div class="bottom-form">
+                <div class="condition">
+                  <label class="input-title">موجودی</label>
+                  <div class="radios">
+                    <div class="available">
+                      <label>
+                        <span>موجود</span>
+                        <input
+                          type="radio"
+                          value="avalibe"
+                          name="condition"
+                          checked
+                        />
+                      </label>
+                    </div>
+                    <div class="unavailable">
+                      <label>
+                        <span>ناموجود</span>
+                        <input
+                          type="radio"
+                          value="unavailable"
+                          name="condition"
+                        />
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <div class="submit-btn">
+                  <input type="submit" value="افزودن" />
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+
       <DataTable title="کاربران">
         <table class="table">
           <thead>
