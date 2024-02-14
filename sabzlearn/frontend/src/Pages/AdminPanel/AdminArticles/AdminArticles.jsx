385e1fa0b16c1d7256f1a0b479af052e73eb5eda
aryaun xames <ariankhamesi@gmail.com>
2024-02-14 17:22:58 +0330

    delete article in admin panel

--- sabzlearn/frontend/src/Pages/AdminPanel/AdminArticles/AdminArticles.jsx
+++ sabzlearn/frontend/src/Pages/AdminPanel/AdminArticles/AdminArticles.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import "./AdminArticles.css"
 import DataTable from '../../../Components/AdminPanel/DataTable/DataTable'
+import swal from "sweetalert"
 
 export default function AdminArticles() {
 
@@ -16,7 +17,33 @@ export default function AdminArticles() {
       .then(result => setAllArticles(result))
   }
 
-  console.log(allArticles);
+  //////////////////////delete article//////////////////////
+
+  const articleRemover = (id) => {
+    swal({
+      title: "از حذف این مقاله اطمینان دارید ؟",
+      icon: "warning",
+      buttons: ["انصراف", "حذف"]
+    })
+      .then(result => {
+        result && remover(id)
+      })
+  }
+
+  const remover = (articleId) => {
+    const localstorageData = JSON.parse(localStorage.getItem("user"))
+    fetch(`http://localhost:5000/v1/articles/${articleId}`, {
+      method: "DELETE",
+      headers: {
+        "Authorization": `Bearer ${localstorageData.token}`
+      }
+    })
+      .then((res) => {
+        res.json()
+        res.ok && swal({ title: "مقاله مدنظر با موفقیت حذف شد", icon: "success", buttons: "بازگشت" })
+      })
+      .then(result => getAllArticles())
+  }
 
   return (
     <>
@@ -31,24 +58,24 @@ export default function AdminArticles() {
               <th>تاریخ ثبت</th>
               <th>ویرایش</th>
               <th>حذف</th>
-              
+
             </tr>
           </thead>
           <tbody>
-            {allArticles.map((article,index )=> (
+            {allArticles.map((article, index) => (
               <tr>
-                <td>{index+1}</td>
+                <td>{index + 1}</td>
                 <td>{article.title}</td>
                 <td>{article.shortName}</td>
                 <td>{article.creator.name}</td>
-                <td>{article.createdAt.slice(0,10)}</td>
+                <td>{article.createdAt.slice(0, 10)}</td>
                 <td>
                   <button type="button" class="btn btn-primary edit-btn">
                     ویرایش
                   </button>
                 </td>
                 <td>
-                  <button type="button" class="btn btn-danger delete-btn" >
+                  <button type="button" class="btn btn-danger delete-btn" onClick={() => articleRemover(article._id)}>
                     حذف
                   </button>
                 </td>
