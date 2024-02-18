a2a85f46b78f665e164114d8467af7d99788c14c
aryaun xames <ariankhamesi@gmail.com>
2024-02-18 14:36:29 +0330

    show user buyed course with filtered menu

--- sabzlearn/frontend/src/Pages/UserPanel/Courses/PanelCourses.jsx
+++ sabzlearn/frontend/src/Pages/UserPanel/Courses/PanelCourses.jsx
@@ -1,9 +1,11 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import "./PanelCourses.css"
 
 export default function PanelCourses() {
 
   const [courses, setCourses] = useState([]);
+  const [showCourseState, setShowCourseState] = useState("all");
+  const [shownCourses, setShownCourses] = useState([]);
   // const [courseInfo, setCourseInfo] = useState({});
 
   useEffect(() => {
@@ -15,10 +17,44 @@ export default function PanelCourses() {
       .then((res) => res.json())
       .then((data) => {
         setCourses(data);
+        setShownCourses(data)
         // setCourseInfo(data.course);
       });
   }, []);
- console.log(courses);
+  //  console.log(courses);
+
+  const filterCourses = (state) => {
+    switch (state) {
+      case "all": {
+        setShownCourses(courses)
+        break
+      }
+
+      case "free": {
+        const filterdCourse = courses.filter(item => item.course.price === 0)
+        setShownCourses(filterdCourse)
+        break
+      }
+
+      case "money": {
+        const filterdCourse = courses.filter(item => item.course.price > 0)
+        setShownCourses(filterdCourse)
+        break
+      }
+
+      case "complete": {
+        const filterdCourse = courses.filter(item => item.course.isComplete === true)
+        setShownCourses(filterdCourse)
+        break
+      }
+
+
+      default: {
+        setShownCourses(courses)
+        break
+      }
+    }
+  }
 
   return (
     <div class="col-9">
@@ -28,19 +64,41 @@ export default function PanelCourses() {
           <ul class="courses-header__list">
             <li class="courses-header__item">
               <a
-                class="courses-header__link courses-header__link-active"
+                className={showCourseState === "all" ? "courses-header__link courses-header__link-active" : "courses-header__link"}
                 href="#"
+                onClick={(event) => {
+                  event.preventDefault()
+                  setShowCourseState("all")
+                  filterCourses("all")
+                }}
               >
                 همه دوره ها
               </a>
             </li>
             <li class="courses-header__item">
-              <a class="courses-header__link" href="#">
-                دوره های فعال
+              <a className={showCourseState === "free" ? "courses-header__link courses-header__link-active" : "courses-header__link"} href="#" onClick={(event) => {
+                event.preventDefault()
+                setShowCourseState("free")
+                filterCourses("free")
+              }}>
+                دوره های رایگان
               </a>
             </li>
             <li class="courses-header__item">
-              <a class="courses-header__link" href="#">
+              <a className={showCourseState === "money" ? "courses-header__link courses-header__link-active" : "courses-header__link"} href="#" onClick={(event) => {
+                event.preventDefault()
+                setShowCourseState("money")
+                filterCourses("money")
+              }}>
+                دوره های پولی
+              </a>
+            </li>
+            <li class="courses-header__item">
+              <a className={showCourseState === "complete" ? "courses-header__link courses-header__link-active" : "courses-header__link"} href="#" onClick={(event) => {
+                event.preventDefault()
+                setShowCourseState("complete")
+                filterCourses("complete")
+              }}>
                 دوره های تکمیل شده
               </a>
             </li>
@@ -49,7 +107,7 @@ export default function PanelCourses() {
         <div class="main">
           <div class="row">
             <div class="col-12">
-              {courses.map((item) => (
+              {shownCourses.map((item) => (
                 <div class="main__box">
                   <div class="main__box-right">
                     <a class="main__box-img-link" href="#">
@@ -66,13 +124,13 @@ export default function PanelCourses() {
                     <div class="main__box-bottom">
                       <div class="main__box-all">
                         <span class="main__box-all-text">وضعیت :</span>
-                        <span class="main__box-all-value">{item.course.isComplete?"تکمیل شده":"در حال برگزاری"}</span>
+                        <span class="main__box-all-value">{item.course.isComplete ? "تکمیل شده" : "در حال برگزاری"}</span>
                       </div>
                       <div class="main__box-completed">
                         <span class="main__box-completed-text">
                           قیمت دوره :
                         </span>
-                        <span class="main__box-completed-value">{item.course.price?item.course.price:"رایگان"}</span>
+                        <span class="main__box-completed-value">{item.course.price ? item.course.price : "رایگان"}</span>
                       </div>
                     </div>
                   </div>
